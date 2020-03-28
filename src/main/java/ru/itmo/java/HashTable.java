b65bdaa0646829b02bc3c63e3e1d05db75bf1f51
kvestus <sasha.chernigin@mail.ru>
2020-03-28 18:33:28 +0300

    success all tests

--- src/main/java/ru/itmo/java/HashTable.java
+++ src/main/java/ru/itmo/java/HashTable.java
@@ -1,23 +1,151 @@
 package ru.itmo.java;
 
 import java.util.Map;
+import java.util.Arrays;
 
 public class HashTable {
 
-    Object put(Object key, Object value) {
-        throw new UnsupportedOperationException();
+    private final static int RESIZE_FACTOR = 2;
+    private final static int PROBING_INTERVAL = 1;
+    private final static float INITIAL_LOAD_FACTOR = 0.5f;
+    private final static int MAX_SIZE = Integer.MAX_VALUE;
+
+    private Entry[] elements;
+    private boolean[] deletedElements;
+    private int size = 0;
+    private int capacity;
+    private float loadFactor;
+    private int threshold;
+
+    public HashTable(int capacity) {
+        this(capacity, INITIAL_LOAD_FACTOR);
     }
 
-    Object get(Object key) {
-        throw new UnsupportedOperationException();
+    public HashTable(int capacity, float loadFactor) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Invalid capacity");
+        }
+
+        if (loadFactor > 1.0f) {
+            throw new IllegalArgumentException("Invalid loadFactor");
+        }
+
+        this.elements = new Entry[capacity];
+        this.deletedElements = new boolean[capacity];
+        this.capacity = capacity;
+        this.loadFactor = loadFactor;
+        this.threshold = (int) Math.floor(capacity * loadFactor);
     }
 
-    Object remove(Object key) {
-        throw new UnsupportedOperationException();
+    public Object put(Object key, Object value) {
+        if (this.size == MAX_SIZE) {
+            throw new RuntimeException("You cannot put more object");
+        }
+
+        Entry newElement = new Entry(key, value);
+        int foundIndex = this.getIndexByKey(key);
+
+        if (this.elements[foundIndex] == null) {
+
+            foundIndex = this.getHash(key);
+            while (this.elements[foundIndex] != null) {
+                foundIndex = (foundIndex + PROBING_INTERVAL) % this.capacity;
+            }
+
+            deletedElements[foundIndex] = false;
+
+            this.elements[foundIndex] = newElement;
+            ++this.size;
+
+            if (this.size >= this.threshold) {
+                this.resize();
+            }
+
+            return null;
+        }
+        Object oldValue = this.elements[foundIndex].value;
+        this.elements[foundIndex] = newElement;
+        return oldValue;
     }
 
-    int size() {
-        throw new UnsupportedOperationException();
+    public Object get(Object key) {
+        Entry foundEntry = this.elements[this.getIndexByKey(key)];
+        return (foundEntry == null) ? null : foundEntry.value;
+    }
+
+    public Object remove(Object key) {
+        int foundIndex = this.getIndexByKey(key);
+
+        if (this.elements[foundIndex] == null) {
+            return null;
+        }
+
+        this.deletedElements[foundIndex] = true;
+        Object oldValue = this.elements[foundIndex].value;
+        --this.size;
+        this.elements[foundIndex] = null;
+        return oldValue;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    private void resize() {
+        if (this.size >= this.threshold) {
+            this.capacity *= RESIZE_FACTOR;
+            this.threshold = (int) (this.capacity * this.loadFactor);
+
+            Entry[] oldElements = this.elements;
+            this.elements = new Entry[this.capacity];
+            this.deletedElements = new boolean[this.capacity];
+            this.size = 0;
+            for (Entry currentElement : oldElements) {
+                if (currentElement != null) {
+                    this.put(currentElement.key, currentElement.value);
+                }
+            }
+        }
+    }
+
+    private int getHash(Object key) {
+        return Math.abs(key.hashCode()) % this.capacity;
+    }
+
+    private int getIndexByKey(Object key) {
+        int hashKey = this.getHash(key);
+        while (deletedElements[hashKey]
+                || (this.elements[hashKey] != null
+                && !this.elements[hashKey].key.equals(key))
+        ) {
+            hashKey = (hashKey + PROBING_INTERVAL) % this.capacity;
+        }
+        return hashKey;
+    }
+
+    private static class Entry {
+
+        private Object key;
+        private Object value;
+
+        public Entry(Object key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public Object getKey() {
+            return this.key;
+        }
+
+        public Object getValue() {
+            return this.value;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("key=%s, value=%s", key.toString(), value.toString());
+        }
+
     }
 
 }
